b099eecc89a57b2071ceb960aa52547f70632f53
Bhavana <138198522+BHAVANAKADAVERU@users.noreply.github.com>
2025-07-15 06:06:20 +0100

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();  // Load environment variables
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -19,20 +21,17 @@ app.use('/patients', patientsRouter);
 app.use('/doctors', doctorsRouter);
 app.use('/appointments', appointmentsRouter);
 
-// MongoDB Connection
-const dbURI = 'mongodb+srv://bhavanakadaveru123:HbLeNuzLvjzUypMn@cluster0.oz0hmv1.mongodb.net/hospital'; // Use 127.0.0.1 instead of localhost to avoid potential IPv6 issues
-mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB Connection using environment variable
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('MongoDB database connection established successfully');
 
-    // Start the server after connecting to the database
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
   })
   .catch((err) => {
     console.error('Error connecting to MongoDB:', err);
-    // Exit the process if MongoDB connection fails
     process.exit(1);
   });
 
